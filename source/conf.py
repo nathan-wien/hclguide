4e8c4773794eae35af61a91937fd158b6803e58d
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2023-02-07 18:55:21 +1000

    initialize fork

---
+++ source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "hclguide"
+copyright = "2018, HashiCorp"
+author = "HashiCorp"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.todo",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "env",
+    ".venv",
+]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+# html_static_path = ["_static"]
+
+# -- Extension configuration -------------------------------------------------
+# todo extension
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
